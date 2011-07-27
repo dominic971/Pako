7c012a76300d951c78876b5b7a6b991883d9427c
dominic <dominic@dominic-K50IJ.(none)>
2011-07-27 18:55:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pako::Application.config.secret_token = '33e8f0afd159a2110a94bcc0e58e2019b7fdf99a0aa4d6ef8497f83ffa5adec46a7c418f3666d14a847838e775e38d5abe3fcf9672394dccc277321499b2bc42'
